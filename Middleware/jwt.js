fd50640c4c7ca2fc6cb2a0de3e5cc81cb5c61fa5
Nanfe05 <hernan05felipe@gmail.com>
2020-06-01 19:25:32 -0500

    Update Middleware

--- Middleware/jwt.js
+++ Middleware/jwt.js
@@ -1,15 +1,13 @@
 const jwt = require('jsonwebtoken');
 
-
-
-
 function TokenMiddleware (req,res,next){
-    const token = req.body.token;
-    const validToken = jwt.verify(token,process.env.SECRETKEY);
-    if(!validToken){
+    try{
+        const token = req.body.token;
+        const validToken = jwt.verify(token,process.env.SECRETKEY);
+        req.user = validToken.user.id;
+    }catch(err){
         return res.status(400).json({errors:[{msg:"Session Expired"}]});
     }
-    req.user = validToken.user.id;
     next();
 };
 
