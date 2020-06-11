81943c8b5044816678d53fda47279ffe7164be32
Nanfe05 <hernan05felipe@gmail.com>
2020-06-10 23:53:04 -0500

    serve landing page to clients

--- Routes/landingPage.js
+++ Routes/landingPage.js
@@ -8,7 +8,38 @@ const LandingPage = require('../Schemas/landingPage');
 // Middlewares
 const middlewareJWT = require('../Middleware/jwt');
 
+
+
+
 // GET
+// SEND CONTENT OF LANDING PAGE
+// Private - Token Needed
+router.get('/',async (req,res)=>{
+    try{
+    let landingPage = await LandingPage.findById(req.query.id);
+    if(landingPage){   
+            res.status(200).json({
+                name:landingPage.name,
+                contents:landingPage.content.objects,
+                sizes:landingPage.content.sizes,
+                success:[{
+                    msg:'Landing Page Loaded'
+                }]
+            });
+    }else{
+        throw Error;
+    }
+        
+    }catch(err){
+        console.log('Error Getting Landing Pages: ',err);
+        return res.status(400).json({errors:[{msg:"Error Getting Landing Page"}]});
+    }
+    
+    
+});
+
+
+// POST
 // SEND AVAILABLE LANDING PAGES
 // Private - Token Needed
 router.post('/getall',middlewareJWT,async (req,res)=>{
@@ -32,6 +63,26 @@ router.post('/getall',middlewareJWT,async (req,res)=>{
     
 });
 
+// POST
+// SEND CONTENT ALREADY SAVED OR LAYOUT
+// Private - Token Needed
+router.post('/content',middlewareJWT,async (req,res)=>{
+    try{
+    const lp = await LandingPage.findById(req.body.id);
+    
+    res.status(200).json({
+        content:lp,
+        success:[{
+            msg:'Content Successfully Loaded'
+        }]
+    });
+    }catch(err){
+        console.log('Error Getting Landing Pages: ',err);
+        return res.status(400).json({errors:[{msg:"Can't get the content right now"}]});
+    }
+    
+    
+});
 
 // POST 
 // Save Landing Page
@@ -44,7 +95,28 @@ router.post('/',middlewareJWT,async (req,res)=>{
         // const landingPage = await LandingPage.findById(req.user);
         let landingPage;
         // Update a Landing Page
-        
+        if(req.body.payload.project_id){
+            const landingPage = await LandingPage.findById(req.body.payload.project_id);
+            // Check that user is the owner of the landing page
+            if(landingPage.userid == req.user){
+                landingPage.name = req.body.payload.lp_name;
+                landingPage.content=req.body.payload;
+
+                await landingPage.save();
+                console.log('Updated');
+                return res.status(200).json({
+                    success:[{
+                        msg:'Landing Page Saved!'
+                    }]
+                });                
+
+            }else{
+                return res.status(400).json({errors:[{msg:"Don't have autorization to edit this file"}]});
+            }
+        }
+
+
+
         // Create a Landing Page
         landingPage = new LandingPage({
             name:req.body.payload.lp_name,
@@ -52,13 +124,13 @@ router.post('/',middlewareJWT,async (req,res)=>{
             useremail:user.email,
             content:req.body.payload,
         });
-
-
         await landingPage.save();
         
-        res.status(200).json({
+        console.log('First Save');
+        return res.status(200).json({
+            id:landingPage._id,
             success:[{
-                msg:'Landing Page Saved'
+                msg:'Landing Page Created!'
             }]
         });
     }catch(err){
