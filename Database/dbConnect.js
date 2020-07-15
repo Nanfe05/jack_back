ecd81b19706991de7aa6a20ee053ef4559ded6b1
Nanfe05 <hernan05felipe@gmail.com>
2020-07-15 09:45:54 -0500

    DB connection feedback

--- Database/dbConnect.js
+++ Database/dbConnect.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 
-function dbConnect (){
+async function dbConnect (){
     try{
          await mongoose.connect(process.env.MONGOURL,{user:process.env.MONGOUSER,pass:process.env.MONGOPASS, useNewUrlParser: true, useUnifiedTopology: true });
         console.log('Data Base Connected');
